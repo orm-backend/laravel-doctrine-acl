4fc75ff839d1d2588a9e60149b413c0013360eb5
Vitaliy Kovalenko <vitalykolsky@gmail.com>
2020-05-19 10:12:24 +0300

    ACL to metadata implemented

--- src/ItAces/ACL/Entities/EntityPermission.php
+++ src/ItAces/ACL/Entities/EntityPermission.php
@@ -5,15 +5,11 @@ use App\Model\Role;
 use App\Model\User;
 use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
 use Illuminate\Support\Facades\Auth;
+use ItAces\ORM\Entities\EntityBase;
 
-abstract class EntityPermission
+abstract class EntityPermission extends EntityBase
 {
-    
-    /**
-     * @var int
-     */
-    protected $id;
-    
+
     /**
      * 
      * @var integer
@@ -31,31 +27,6 @@ abstract class EntityPermission
      * @var string
      */
     protected $model;
-    
-    /**
-     * @var \Carbon\Carbon
-     */
-    protected $createdAt;
-    
-    /**
-     * @var \Carbon\Carbon|null
-     */
-    protected $updatedAt;
-    
-    /**
-     * @var \Carbon\Carbon|null
-     */
-    protected $deletedAt;
-    
-    /**
-     * @var \App\Model\User
-     */
-    protected $createdBy;
-    
-    /**
-     * @var \App\Model\User
-     */
-    protected $updatedBy;
     
     /**
      * @return integer
@@ -105,119 +76,4 @@ abstract class EntityPermission
         $this->model = $model;
     }
 
-    /**
-     * Set createdAt.
-     *
-     * @param \Carbon\Carbon $createdAt
-     */
-    public function setCreatedAt($createdAt)
-    {
-        $this->createdAt = $createdAt;
-    }
-    
-    /**
-     * Get createdAt.
-     *
-     * @return \Carbon\Carbon
-     */
-    public function getCreatedAt()
-    {
-        return $this->createdAt;
-    }
-    
-    /**
-     * Set updatedAt.
-     *
-     * @param \Carbon\Carbon|null $updatedAt
-     */
-    public function setUpdatedAt($updatedAt = null)
-    {
-        $this->updatedAt = $updatedAt;
-    }
-    
-    /**
-     * Set createdBy.
-     *
-     * @param \App\Model\User $createdBy
-     */
-    public function setCreatedBy(User $createdBy)
-    {
-        $this->createdBy = $createdBy;
-    }
-    
-    /**
-     * Get createdBy.
-     *
-     * @return \App\Model\User
-     */
-    public function getCreatedBy()
-    {
-        return $this->createdBy;
-    }
-    
-    /**
-     *
-     * @param \Doctrine\Common\Persistence\Event\LifecycleEventArgs $event
-     */
-    public function onBeforeAdd(LifecycleEventArgs $event)
-    {
-        $this->createdAt = now();
-        
-        if (Auth::id()) {
-            $this->createdBy = Auth::user();
-        }
-        
-        $this->validate();
-    }
-    
-    /**
-     *
-     * @param \Doctrine\Common\Persistence\Event\LifecycleEventArgs $event
-     */
-    public function onAfterAdd(LifecycleEventArgs $event)
-    {
-        // Add your code here
-    }
-    
-    /**
-     *
-     * @param \Doctrine\Common\Persistence\Event\LifecycleEventArgs $event
-     */
-    public function onBeforeUpdate(LifecycleEventArgs $event)
-    {
-        $this->updatedAt = now();
-        
-        if (Auth::id()) {
-            $this->updatedBy = Auth::user();
-        }
-        
-        $this->validate();
-    }
-    
-    /**
-     *
-     * @param \Doctrine\Common\Persistence\Event\LifecycleEventArgs $event
-     */
-    public function onAfterUpdate(LifecycleEventArgs $event)
-    {
-        // Add your code here
-    }
-    
-    /**
-     *
-     * @param \Doctrine\Common\Persistence\Event\LifecycleEventArgs $event
-     */
-    public function onBeforeDelete(LifecycleEventArgs $event)
-    {
-        // Add your code here
-    }
-    
-    /**
-     *
-     * @param \Doctrine\Common\Persistence\Event\LifecycleEventArgs $event
-     */
-    public function onAfterDelete(LifecycleEventArgs $event)
-    {
-        // Add your code here
-    }
 }
